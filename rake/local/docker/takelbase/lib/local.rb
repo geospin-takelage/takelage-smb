b632e0a03eec4796b6cbadfabcbde18f38df6cea
Stephan Wehrheim <swehrheim@geospin.de>
2020-08-04 12:56:30 +0200

    tau pull

--- rake/local/docker/takelbase/lib/local.rb
+++ rake/local/docker/takelbase/lib/local.rb
@@ -16,11 +16,20 @@ require 'rake'
   'molecule dependency --scenario-name local'
 
 @cmd_local_docker_takelbase_molecule_log = 'docker logs -f ' \
-  "molecule-takel-#{@project['project']}-local-%<project_environment>s"
+  "molecule-takel-#{@project['name']}-local-%<project_environment>s"
+
+@cmd_local_docker_takelbase_molecule_list_command = "cd ansible && bash -c '" \
+  'TAKELAGE_DOCKER_CMD_OVERRIDE=%<docker_command_override>s ' \
+  'TAKELAGE_DOCKER_CMD=%<docker_command>s ' \
+  'TAKELAGE_DOCKER_MOUNT_PATH=%<docker_mount_path>s ' \
+  'TAKELAGE_DOCKER_TTY=%<docker_tty>s ' \
+  'TAKELAGE_PROJECT_ENV=%<project_environment>s ' \
+  "molecule %<molecule_command>s'"
 
 @cmd_local_docker_takelbase_list = {
   converge: @cmd_local_docker_takelbase_molecule_command,
   destroy: @cmd_local_docker_takelbase_molecule_command,
+  list: @cmd_local_docker_takelbase_molecule_list_command,
   login: @cmd_local_docker_takelbase_molecule_command,
   verify: @cmd_local_docker_takelbase_molecule_command,
   features: @cmd_local_docker_takelbase_molecule_features,
