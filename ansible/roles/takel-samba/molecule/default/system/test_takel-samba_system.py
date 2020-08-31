d3483d797286599bd667730c09560eab1690e279
Cucumber <cucumber@example.com>
2020-08-31 17:25:38 +0200

    Configure takel-samba

---
+++ ansible/roles/takel-samba/molecule/default/system/test_takel-samba_system.py
@@ -0,0 +1,8 @@
+import takeltest
+
+testinfra_hosts = takeltest.hosts()
+
+
+def test_takel_samba_system_smbclient_connect(host):
+    command = 'smbclient --no-pass --list=127.0.0.1'
+    assert host.run_expect([0], command)
