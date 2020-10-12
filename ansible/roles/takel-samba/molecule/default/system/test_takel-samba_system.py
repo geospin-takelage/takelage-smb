861cc47711b4c1327c38e13aec8ef029a705fd2d
Alexander Hansen <ahansen@geospin.de>
2020-10-12 14:08:02 +0200

    Rename test for takel-samba

--- ansible/roles/takel-samba/molecule/default/system/test_takel-samba_system.py
+++ ansible/roles/takel-samba/molecule/default/system/test_takel-samba_system.py
@@ -3,6 +3,6 @@ import takeltest
 testinfra_hosts = takeltest.hosts()
 
 
-def test_takel_samba_system_smbclient_connect(host):
+def test_takel_samba_system_smbclient_connect_to_smbd(host):
     command = 'smbclient --no-pass --list=127.0.0.1'
     assert host.run_expect([0], command)
